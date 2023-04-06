9411a5dd92def73eaa612c0a54d603c9233082b6
Davi-Motta <68289182+Davi-Motta@users.noreply.github.com>
2023-04-06 15:14:20 -0300

    removed a couple debug lines

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
-git clone --depth 1 https://github.com/Davi-Motta/nvim-config\
- ~/.config/nvim
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
