9f2c48cff8e3e30202694967bc50e5432b8995d5
Davi-Motta <68289182+Davi-Motta@users.noreply.github.com>
2023-04-06 15:19:05 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 nvim -c :'PackerSync'
-rm setup.sh
+rm ~/.config/nvim/setup.sh
